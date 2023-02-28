c676caeb09f0a21220e0b3f134f0dec23fe35258
ilayabarathisdb <124340388+ilayabarathisdb@users.noreply.github.com>
2023-02-28 18:46:37 +0530

    [Feat:] Popup

---
+++ lib/globalaccess/popup_message/popup.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../applib/sizer.dart';
+
+class Popup extends StatefulWidget {
+  const Popup({super.key});
+
+  @override
+  State<Popup> createState() => _PopupState();
+}
+
+
+class _PopupState extends State<Popup> {
+  @override
+  
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+    return  AlertDialog(
+                  title: const Text("Alert Dialog Box"),
+                  content: const Text("You have raised a Alert Dialog Box"),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                      //  Navigator.of(ctx).pop();
+                      },
+                      child: Container(
+                        color: Colors.green,
+                        padding: const EdgeInsets.all(14),
+                        child: const Text("okay"),
+                      ),
+                    ),
+                  ],
+                
+
+
+    );
+  });}
+}
